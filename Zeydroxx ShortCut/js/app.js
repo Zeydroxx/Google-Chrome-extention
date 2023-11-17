0f68c9f9c813473b4a8ffcf8ec6639894ac70ff1
Zeydroxx <149490069+Zeydroxx@users.noreply.github.com>
2023-11-17 21:15:07 +0100

    Add files via upload

---
+++ Zeydroxx ShortCut/js/app.js
@@ -0,0 +1,40 @@
+$('#Amazon').click(function(){
+    chrome.tabs.create({ url: 'https://www.amazon.com' });
+});
+
+$('#Discord').click(function(){
+    chrome.tabs.create({ url: 'https://www.discord.com' });
+});
+
+$('#Github').click(function(){
+    chrome.tabs.create({ url: 'https://www.github.com' });
+});
+
+$('#Instagram').click(function(){
+    chrome.tabs.create({ url: 'https://www.instagram.com' });
+});
+
+
+$('#JustDevThat').click(function(){
+    chrome.tabs.create({ url: 'https://www.justdevthat.fr' });
+});
+
+$('#MediaFire').click(function(){
+    chrome.tabs.create({ url: 'https://www.mediafire.com' });
+});
+
+$('#Netflix').click(function(){
+    chrome.tabs.create({ url: 'https://www.netflix.fr' });
+});
+
+$('#TikTok').click(function(){
+    chrome.tabs.create({ url: 'https://www.tikTok.com' });
+});
+
+$('#Twitch').click(function(){
+    chrome.tabs.create({ url: 'https://www.twitch.com' });
+});
+
+$('#YT').click(function(){
+    chrome.tabs.create({ url: 'https://www.youtube.com' });
+});
